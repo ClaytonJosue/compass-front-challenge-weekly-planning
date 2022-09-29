d6af0c7898f7a33764702be444598afea663a16b
ClaytonJosue <jogamesbr123@gmail.com>
2022-09-29 12:08:16 -0300

    added functional weeks

---
+++ assets/scripts/activities.js
@@ -0,0 +1,148 @@
+const mondayButton = document.querySelector(".week__days--item-segunda");
+const tuesdayButton = document.querySelector(".week__days--item-terca");
+const wednesdayButton = document.querySelector(".week__days--item-quarta");
+const thursdayButton = document.querySelector(".week__days--item-quinta");
+const fridayButton = document.querySelector(".week__days--item-sexta");
+const saturdayButton = document.querySelector(".week__days--item-sabado");
+const sundayButton = document.querySelector(".week__days--item-domingo");
+
+const monday = document.querySelector(".monday");
+const tuesday = document.querySelector(".tuesday");
+const wednesday = document.querySelector(".wednesday");
+const thursday = document.querySelector(".thursday");
+const friday = document.querySelector(".friday");
+const saturday = document.querySelector(".saturday");
+const sunday = document.querySelector(".sunday");
+
+// Monday
+mondayButton.addEventListener('click', () => {
+	mondayButton.setAttribute('style', 'transform: scale(1.2);');
+	tuesdayButton.setAttribute('style', 'transform: scale(1);');
+	wednesdayButton.setAttribute('style', 'transform: scale(1);');
+	thursdayButton.setAttribute('style', 'transform: scale(1);');
+	fridayButton.setAttribute('style', 'transform: scale(1);');
+	saturdayButton.setAttribute('style', 'transform: scale(1);');
+	sundayButton.setAttribute('style', 'transform: scale(1);');
+
+	monday.classList.remove('hidden');
+	tuesday.classList.add('hidden');
+	wednesday.classList.add('hidden');
+	thursday.classList.add('hidden');
+	friday.classList.add('hidden');
+	saturday.classList.add('hidden');
+	sunday.classList.add('hidden');
+});
+
+// Tuesday
+tuesdayButton.addEventListener('click', () => {
+	mondayButton.setAttribute('style', 'transform: scale(1);');
+	tuesdayButton.setAttribute('style', 'transform: scale(1.2);');
+	wednesdayButton.setAttribute('style', 'transform: scale(1);');
+	thursdayButton.setAttribute('style', 'transform: scale(1);');
+	fridayButton.setAttribute('style', 'transform: scale(1);');
+	saturdayButton.setAttribute('style', 'transform: scale(1);');
+	sundayButton.setAttribute('style', 'transform: scale(1);');
+
+	monday.classList.add('hidden');
+	tuesday.classList.remove('hidden');
+	wednesday.classList.add('hidden');
+	thursday.classList.add('hidden');
+	friday.classList.add('hidden');
+	saturday.classList.add('hidden');
+	sunday.classList.add('hidden');
+});
+
+// Wednesday
+wednesdayButton.addEventListener('click', () => {
+	mondayButton.setAttribute('style', 'transform: scale(1);');
+	tuesdayButton.setAttribute('style', 'transform: scale(1);');
+	wednesdayButton.setAttribute('style', 'transform: scale(1.2);');
+	thursdayButton.setAttribute('style', 'transform: scale(1);');
+	fridayButton.setAttribute('style', 'transform: scale(1);');
+	saturdayButton.setAttribute('style', 'transform: scale(1);');
+	sundayButton.setAttribute('style', 'transform: scale(1);');
+
+	monday.classList.add('hidden');
+	tuesday.classList.add('hidden');
+	wednesday.classList.remove('hidden');
+	thursday.classList.add('hidden');
+	friday.classList.add('hidden');
+	saturday.classList.add('hidden');
+	sunday.classList.add('hidden');
+});
+
+// Thursday
+thursdayButton.addEventListener('click', () => {
+	mondayButton.setAttribute('style', 'transform: scale(1);');
+	tuesdayButton.setAttribute('style', 'transform: scale(1);');
+	wednesdayButton.setAttribute('style', 'transform: scale(1);');
+	thursdayButton.setAttribute('style', 'transform: scale(1.2);');
+	fridayButton.setAttribute('style', 'transform: scale(1);');
+	saturdayButton.setAttribute('style', 'transform: scale(1);');
+	sundayButton.setAttribute('style', 'transform: scale(1);');
+
+	monday.classList.add('hidden');
+	tuesday.classList.add('hidden');
+	wednesday.classList.add('hidden');
+	thursday.classList.remove('hidden');
+	friday.classList.add('hidden');
+	saturday.classList.add('hidden');
+	sunday.classList.add('hidden');
+});
+
+// Friday
+fridayButton.addEventListener('click', () => {
+	mondayButton.setAttribute('style', 'transform: scale(1);');
+	tuesdayButton.setAttribute('style', 'transform: scale(1);');
+	wednesdayButton.setAttribute('style', 'transform: scale(1);');
+	thursdayButton.setAttribute('style', 'transform: scale(1);');
+	fridayButton.setAttribute('style', 'transform: scale(1.2);');
+	saturdayButton.setAttribute('style', 'transform: scale(1);');
+	sundayButton.setAttribute('style', 'transform: scale(1);');
+
+	monday.classList.add('hidden');
+	tuesday.classList.add('hidden');
+	wednesday.classList.add('hidden');
+	thursday.classList.add('hidden');
+	friday.classList.remove('hidden');
+	saturday.classList.add('hidden');
+	sunday.classList.add('hidden');
+});
+
+// Saturday
+saturdayButton.addEventListener('click', () => {
+	mondayButton.setAttribute('style', 'transform: scale(1);');
+	tuesdayButton.setAttribute('style', 'transform: scale(1);');
+	wednesdayButton.setAttribute('style', 'transform: scale(1);');
+	thursdayButton.setAttribute('style', 'transform: scale(1);');
+	fridayButton.setAttribute('style', 'transform: scale(1);');
+	saturdayButton.setAttribute('style', 'transform: scale(1.2);');
+	sundayButton.setAttribute('style', 'transform: scale(1);');
+
+	monday.classList.add('hidden');
+	tuesday.classList.add('hidden');
+	wednesday.classList.add('hidden');
+	thursday.classList.add('hidden');
+	friday.classList.add('hidden');
+	saturday.classList.remove('hidden');
+	sunday.classList.add('hidden');
+});
+
+// Sunday
+sundayButton.addEventListener('click', () => {
+	mondayButton.setAttribute('style', 'transform: scale(1);');
+	tuesdayButton.setAttribute('style', 'transform: scale(1);');
+	wednesdayButton.setAttribute('style', 'transform: scale(1);');
+	thursdayButton.setAttribute('style', 'transform: scale(1);');
+	fridayButton.setAttribute('style', 'transform: scale(1);');
+	saturdayButton.setAttribute('style', 'transform: scale(1);');
+	sundayButton.setAttribute('style', 'transform: scale(1.2);');
+
+	monday.classList.add('hidden');
+	tuesday.classList.add('hidden');
+	wednesday.classList.add('hidden');
+	thursday.classList.add('hidden');
+	friday.classList.add('hidden');
+	saturday.classList.add('hidden');
+	sunday.classList.remove('hidden');
+});
